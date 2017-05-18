56bb30b378a0eae961b5d8aac6c693382cf6ebe0
jibo <jibo@outlook.com>
2017-05-18 11:02:10 +0800

    Update database

--- sql/db.sql
+++ sql/db.sql
@@ -19,7 +19,6 @@ DROP TABLE IF EXISTS store;
 CREATE TABLE store (
   id int(20) unsigned NOT NULL AUTO_INCREMENT,
   name varchar(100) NOT NULL,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(id)
 );
 
@@ -28,6 +27,68 @@ CREATE TABLE brand (
   id int(20) unsigned NOT NULL AUTO_INCREMENT,
   store_id int(20) unsigned NOT NULL,
   name varchar(100) NOT NULL,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS item_type;
+CREATE TABLE item_type (
+  id int(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS closure;
+CREATE TABLE closure (
+  id int(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS heel;
+CREATE TABLE heel (
+  id int(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS height_map;
+CREATE TABLE height_map (
+  id int(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS material;
+CREATE TABLE material (
+  id int(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS occasion;
+CREATE TABLE occasion (
+  id int(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS pattern;
+CREATE TABLE pattern (
+  id int(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS strap;
+CREATE TABLE strap (
+  id int(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS toe;
+CREATE TABLE toe (
+  id int(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
   PRIMARY KEY(id)
 );
