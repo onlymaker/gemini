79063a6ffa5c9b0cfe5c75c0f83d2002a824ac3a
jibo <jibo@outlook.com>
2017-05-19 13:51:43 +0800

    Config page of item type

---
+++ src/app/system/ItemType.php
@@ -0,0 +1,31 @@
+<?php
+namespace app\system;
+
+use data\Database;
+use DB\SQL\Mapper;
+
+class ItemType extends SysBase
+{
+    function get($f3)
+    {
+        $mapper = new Mapper(Database::mysql(), 'item_type');
+        $f3->set('title', '产品类型管理');
+        $f3->set('results', $mapper->find());
+        echo \Template::instance()->render('system/item_type.html');
+    }
+
+    function post($f3)
+    {
+        $name = $_POST['name'];
+        $itemType = new Mapper($this->db, 'item_type');
+        $itemType->load(["name = ?", $name]);
+        if ($itemType->dry()) {
+            $f3->log('Create item type ' . $name);
+            $itemType['name'] = strtoupper($name);
+            $itemType->save();
+            echo 'SUCCESS';
+        } else {
+            echo 'EXISTED';
+        }
+    }
+}
