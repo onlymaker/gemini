6c3cf5c957ee79800b84020dd8a2db446a810e90
jibo <jibo@outlook.com>
2017-05-20 16:25:35 +0800

    Search by model

--- src/app/Index.php
+++ src/app/Index.php
@@ -2,6 +2,7 @@
 namespace app;
 
 use app\common\AppBase;
+use DB\SQL\Mapper;
 
 class Index extends AppBase
 {
@@ -9,5 +10,23 @@ class Index extends AppBase
     {
         $f3->set('title', '首页');
         echo \Template::instance()->render('index.html');
+    }
+
+    function search($f3)
+    {
+        $mapper = new Mapper($this->db, 'raw');
+        $results = $mapper->find(['model like ?', '%' . $_GET['model'] . '%']);
+        $data = [];
+        foreach ($results as $result) {
+            $data[] = [
+                'id' => $result['id'],
+                'user' => $result['user'],
+                'model' => $result['model'],
+                'create_time' => $result['create_time']
+            ];
+        }
+        $f3->log($this->db->log());
+        $this->error['code'] = 0;
+        echo $this->jsonResponse(['results' => $data]);
     }
 }
